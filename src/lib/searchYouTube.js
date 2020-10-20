b405a4b7008d32c6cfb11067e3abd51581589880
Lillian Eng <lillian.eng88@gmail.com>
2020-10-20 16:01:08 -0700

    "applied Ajax to for getting youtube live data"

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,20 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+var searchYouTube = ({key, query, max = 5}, callback) => {
+  $.get('https://www.googleapis.com/youtube/v4/searach', {
+    part: 'snippet',
+    key: key,
+    q: query,
+    maxResults: max,
+    type: 'video',
+    videoEmbeddable: 'true'
+  })
+    .done (({items}) => {
+      if (callback) {
+        callback(items);
+      }
+    })
+    .fail(({responseJSON}) => {
+      responseJSON.error.errors.forEach((err) => console.error(err));
+    });
 };
 
 export default searchYouTube;
